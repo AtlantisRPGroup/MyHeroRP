e28784f398a7355c121c5c7f58df856798939204
AtlantisRPGroup <atlantisrp.corporation@gmail.com>
2025-05-16 01:05:25 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://www.dropbox.com/scl/fi/2120b7fzjc0gu4a2yl8oa/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://dl.dropboxusercontent.com/s/2120b7fzjc0gu4a2yl8oa/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
